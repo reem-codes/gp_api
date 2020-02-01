b7b5cb7c2fbf6a83d01285f271a3641a8d2fc16f
reem-codes <reem.brain@gmail.com>
2020-02-01 19:24:12 +0300

    weeb 2: models intro

--- config.example.py
+++ config.example.py
@@ -1,2 +1,5 @@
 class Config:
-    name = "enter your name here"
+    SQLALCHEMY_DATABASE_URI = "postgres://username:password@localhost/dbname"
+    SQLALCHEMY_NATIVE_UNICODE = "utf-8"
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SECRET_KEY = "secret key"
